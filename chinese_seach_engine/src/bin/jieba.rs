9bb467a4fbdfc241937e374df1be0511e3375be2
maol <maol@nanofilm.com.cn>
2024-04-01 10:56:10 +0800

    手动实现一个简化版的`jieba`分词工具

--- chinese_seach_engine/src/bin/jieba.rs
+++ chinese_seach_engine/src/bin/jieba.rs
@@ -1,6 +1,5 @@
 use std::{
     collections::HashMap,
-    f32::consts::E,
     fs::File,
     io::{self, BufRead},
 };
@@ -8,6 +7,7 @@ use std::{
 // 构建词典
 fn build_dict() -> HashMap<String, f32> {
     let mut dict_words = HashMap::new();
+    dict_words.insert("世界".to_string(), 0.5);
     dict_words.insert("经济".to_string(), 0.5);
     dict_words.insert("经济学".to_string(), 0.8);
     dict_words.insert("是".to_string(), 0.2);
@@ -43,7 +43,41 @@ fn max_forward_matching(
     dict_words: HashMap<String, f32>,
     max_word_length: usize,
 ) -> Vec<String> {
-    Vec::new()
+    let _ = dict_words;
+    let mut words = Vec::new();
+    let mut start = 0;
+
+    while start < text.chars().count() {
+        let mut matched = false;
+        let mut end = start + max_word_length;
+
+        if end > text.chars().count() {
+            end = text.chars().count();
+        }
+
+        while start < end {
+            let word = text
+                .chars()
+                .skip(start)
+                .take(end - start)
+                .collect::<String>();
+            if dict_words.contains_key(&word) {
+                words.push(word.clone());
+                start = end;
+                matched = true;
+                break;
+            }
+            end -= 1;
+        }
+
+        if !matched {
+            let single_char: String = text.chars().nth(start).unwrap().to_string();
+            words.push(single_char);
+            start += 1;
+        }
+    }
+
+    words
 }
 
 fn main() {
@@ -53,5 +87,36 @@ fn main() {
     if let Err(e) = load_user_dict("chinese_seach_engine/src/user_dict.txt", &mut dict_words) {
         eprintln!("Failed to load user dictionary: {}", e);
     }
-    println!("{:?}", dict_words);
+    let s = max_forward_matching(text, dict_words, 5);
+    println!("{:?}", s);
+}
+
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_range() {
+        for i in (1..=5).rev().step_by(1) {
+            println!("{}", i);
+        }
+
+        let s = "hello world";
+        println!("{}", s.len());
+        let _slice = &s[0..5]; // slice 包含 "hello"
+                               // assert_eq!(slice, "hello");
+                               // assert_eq!(s.len(), 11);
+
+        let text = "世界经济学是一门非常大的社会科学";
+        println!("{}", text.chars().count());
+        println!("{}", &text[3..6]);
+    }
+
+    #[test]
+    fn test_max_forward_matching() {
+        let text = "世界经济学是一门非常大的社会科学";
+        let dict_words = build_dict();
+
+        let res = max_forward_matching(text, dict_words, 5);
+        println!("{:?}", res);
+    }
 }
--- chinese_seach_engine/src/main.rs
+++ chinese_seach_engine/src/main.rs
@@ -1,3 +1,46 @@
-fn main() {
-    println!("Hello, world!");
+use jieba_rs::Jieba;
+
+
+fn main() {}
+
+mod tests {
+    #[allow(unused_imports)]
+    use jieba_rs::{Jieba, KeywordExtract, TextRank, TFIDF};
+
+    #[test]
+    fn it_works() {
+        let jieba = Jieba::new();
+        let text = "世界经济学是一门非常大的社会科学";
+        let words = jieba.cut(text, true);
+        println!("{:?}", words);
+        let words = jieba.cut_all(text);
+        println!("{:?}", words);
+        let words = jieba.cut_for_search(text, true);
+        println!("{:?}", words);
+        // assert_eq!(words, vec!["我们", "中", "出", "了", "一个", "叛徒"]);
+        // ["世界", "经济学", "是", "一门", "非常", "大", "的", "社会科学"]
+        // ["世", "世界", "界", "经", "经济", "经济学", "济", "济学", "学", "是", "一", "一门", "门", "非", "非常", "常", "大", "的", "社", "社会", "社会科学", "会", "科", "科学", "学"]
+        // ["世界", "经济", "济学", "经济学", "是", "一门", "非常", "大", "的", "社会", "科学", "社会科学"]
+        println!();
+
+        let keyword_extractor = TFIDF::new_with_jieba(&jieba);
+        let top_k = keyword_extractor.extract_tags(
+            "今天纽约的天气真好啊",
+            1,
+            vec![],
+        );
+        println!("TFIDF：{:?}", top_k);
+
+        let keyword_extractor = TextRank::new_with_jieba(&jieba);
+        let top_k = keyword_extractor.extract_tags(
+            "此外，公司拟对全资子公司吉林欧亚置业有限公司增资4.3亿元，增资后，吉林欧亚置业注册资本由7000万元增加到5亿元。吉林欧亚置业主要经营范围为房地产开发及百货零售等业务。目前在建吉林欧亚城市商业综合体项目。2013年，实现营业收入0万元，实现净利润-139.13万元。",
+            6,
+            vec![String::from("ns"), String::from("n"), String::from("vn"), String::from("v")],
+        );
+        println!("KeywordExtract：{:?}", top_k);
+
+        // KeywordExtract：这是 jieba_rs 库中的一个特征提取器，用于从文本中提取关键词。它使用了 TextRank 算法，这是一种基于图排序的算法，可以用来发现文本中最重要的单词或短语。
+        // TextRank：这是 TextRank 算法的实现，它可以用于关键词提取或自动摘要生成。TextRank 算法通过构建一个基于词频和词之间关系的图，然后使用图排序算法（如 PageRank）来确定文本中各个词的重要性。
+        // TFIDF：这是词频-逆文档频率（Term Frequency-Inverse Document Frequency）的实现，它是一种用于信息检索和文本挖掘的常用加权技术。TFIDF 有助于评估一个词在文档集合中的重要性。
+    }
 }
