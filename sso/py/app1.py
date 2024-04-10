3fb6e409b632f6c28e15c955c2dd00c4d59c9a2f
maolei <maolei025@qq.com>
2024-04-11 00:41:11 +0800

    ✨ feat(sso): add jwt

--- sso/py/app1.py
+++ sso/py/app1.py
@@ -1,84 +1,91 @@
-from flask import Flask, request, redirect, jsonify
+from functools import wraps
+from flask import Flask, g, request, redirect, jsonify
 import requests
+from jwt import decode, ExpiredSignatureError, InvalidTokenError
+import jwt
 
 app = Flask(__name__)
 # 假设用户信息存储
 users_info = {
-    'user1': {
-        'email': 'user1@example.com',
-        'password': '123',
-        'name': 'User 1'
-    },
-    'user2': {
-        'email': 'user2@example.com',
-        'password': '123',
-        'name': 'User 2'
-    }
+    "user1": {"email": "user1@example.com", "password": "123", "name": "User 1"},
+    "user2": {"email": "user2@example.com", "password": "123", "name": "User 2"},
 }
 
-@app.route('/logout')
+JWT_SECRET = "sso-3E0C07FFFCFFF3E00E0039FCE00E7F387"  # 加解密密钥
+JWT_ALGORITHM = "HS256"  # 加解密算法
+
+def token_required(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        token = request.cookies.get("auth_token")
+        if not token:
+            # 如果没有Token，重定向到登录页面
+            return redirect(
+                "http://localhost:8000/login?redirect_url=http://localhost:8001"
+            )
+
+        try:
+            payload = jwt.decode(token, JWT_SECRET, algorithms=[JWT_ALGORITHM])
+            g.username = payload["username"]
+            # 你可以在这里添加更多的验证逻辑，如验证JWT中的claims
+        except (ExpiredSignatureError, InvalidTokenError):
+            # 如果Token无效或已过期，重定向到登录页面
+            return redirect(
+                "http://localhost:8000/login?redirect_url=http://localhost:8001"
+            )
+
+        return f(*args, **kwargs)
+
+    return decorated
+
+
+@app.route("/logout")
+@token_required
 def logout():
     # 假设从某处获取到了Token，例如从session或者直接从请求中
-    token = request.cookies.get('auth_token')
+    token = request.cookies.get("auth_token")
     # 向认证中心发送登出请求
-    headers = {'Authorization': token}
-    response = requests.get('http://localhost:8000/logout', headers=headers)
+    headers = {"Authorization": token}
+    response = requests.get("http://localhost:8000/logout", headers=headers)
 
     if response.status_code == 200:
         return f"Logout successfully!"
     else:
         # Token无效，重定向到登录页面
-        return redirect('http://localhost:8000/login?redirect_url=http://localhost:8001')
+        return redirect(
+            "http://localhost:8000/login?redirect_url=http://localhost:8001"
+        )
 
-@app.route('/')
+
+@app.route("/")
+@token_required
 def home():
-    # token = request.args.get('Authorization', None)
-    token = request.cookies.get('auth_token')
-    if token:
-        # 验证Token
-        # response = requests.get('http://localhost:8000/verify', params={'token': token})
-        headers = {'Authorization': token}
-        response = requests.get('http://localhost:8000/verify', headers=headers)
-        if response.status_code == 200 and response.json()['status'] == 'valid':
-            username = response.json().get('username')
-            return f"This is app1, Welcome {username}! You are logged in."
-        else:
-            # Token无效，重定向到登录页面
-            return redirect('http://localhost:8000/login?redirect_url=http://localhost:8001')
-    else:
-        # 没有Token，重定向到登录页面
-        return redirect('http://localhost:8000/login?redirect_url=http://localhost:8001')
+    username = g.username
+    return f"This is app1, Welcome {username}! You are logged in."
 
-@app.route('/profile')
+
+
+@app.route("/profile")
+@token_required
 def profile():
-    # token = request.args.get('token', None)
-    token = request.cookies.get('auth_token')
-    if token:
-        # 验证Token
-        headers = {'Authorization': token}
-        response = requests.get('http://localhost:8000/verify', headers=headers)
-        if response.status_code == 200:
-            username = response.json().get('username')
-            user_info = users_info.get(username, {})
-            return jsonify(user_info)
-        else:
-            return jsonify({"error": "Invalid or expired token."}), 403
-    else:
-        return jsonify({"error": "No token provided."}), 401
+    username = g.username
+    user_info = users_info.get(username, {})
+    return jsonify(user_info)
 
-@app.route('/change-password', methods=['POST'])
+
+@app.route("/change-password", methods=["POST"])
 def change_password():
-    token = request.args.get('token', None)
-    new_password = request.form.get('new_password', None)
+    token = request.args.get("token", None)
+    new_password = request.form.get("new_password", None)
 
     if token and new_password:
         # 验证Token
-        response = requests.get('http://localhost:8000/verify', params={'token': token})
+        response = requests.get("http://localhost:8000/verify", params={"token": token})
         if response.status_code == 200:
-            username = response.json().get('username')
+            username = response.json().get("username")
             # 简化示例，直接更新内存中的密码
             if username in users_info:
-                users_info[username]['password'] = new_password
+                users_info[username]["password"] = new_password
                 return jsonify({"message": "Password changed successfully."})
             else:
                 return jsonify({"error": "User not found."}), 404
@@ -87,5 +94,6 @@ def change_password():
     else:
         return jsonify({"error": "Missing token or new password."}), 400
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8001)
--- sso/py/idp.py
+++ sso/py/idp.py
@@ -1,23 +1,51 @@
 from datetime import datetime, timedelta
 import logging
+import time
 from uuid import uuid4
 from flask import Flask, jsonify, request, redirect, make_response
 import jwt
 
+JWT_TOKEN_EXPIRE_TIME = 3600 * 2  # token有效时间 2小时
+JWT_SECRET = "sso-3E0C07FFFCFFF3E00E0039FCE00E7F387"  # 加解密密钥
+JWT_ALGORITHM = "HS256"  # 加解密算法
 # 设置日志
-logging.basicConfig(level=logging.INFO, filename='sso.log',
-                    format='%(asctime)s - %(levelname)s - %(message)s')
+logging.basicConfig(
+    level=logging.INFO,
+    filename="sso.log",
+    format="%(asctime)s - %(levelname)s - %(message)s",
+)
 
 app = Flask(__name__)
-app.secret_key = 'idp-7F39F01FCE7FFE0318001C0670070E03F'
+app.secret_key = "idp-7F39F01FCE7FFE0318001C0670070E03F"
 
-sso_secret_key = 'sso-3E0C07FFFCFFF3E00E0039FCE00E7F387'
 
-TOKEN_EXPIRATION_MINUTES = 30
-
-tokens = {}
+tokens = []
 # 存储用户名和密码（示例用，实际应用中应使用数据库）
-users = {"user1": "123","user2": "123"}
+users = {"user1": "123", "user2": "123"}
+
+
+def generate_jwt_token(username: str) -> str:
+    """根据用户id生成token"""
+    payload = {"username": username, "exp": int(time.time()) + JWT_TOKEN_EXPIRE_TIME}
+    token = jwt.encode(payload, JWT_SECRET, algorithm=JWT_ALGORITHM)
+    return token
+
+
+def verify_jwt_token(username: str, token: str) -> bool:
+    """验证用户token"""
+    payload = {"username": username}
+    try:
+        _payload = jwt.decode(token, JWT_SECRET, algorithms=[JWT_ALGORITHM])
+    except jwt.PyJWTError:
+        logging.info("token verification failed")
+        return False
+    else:
+        logging.info(_payload)
+        exp = int(_payload.pop("exp"))
+        if time.time() > exp:
+            logging.info("token expired")
+            return False
+        return payload == _payload
 
 
 @app.route("/login", methods=["GET", "POST"])
@@ -30,12 +58,9 @@ def login():
 
         if users.get(username) == password:  # 简单的认证逻辑
             logging.info(f"User {username} logged in.")
-            
-            token = str(uuid4())  # 实际应用中应生成唯一Token
-            # token = jwt.encode({'username': username}, sso_secret_key, algorithm='HS256')
-            
-            token_issue_time = datetime.now()
-            tokens[token] = {"username": username, "issue_time": token_issue_time}
+
+            token = generate_jwt_token(username)
+            tokens.append(token)
             # 登录成功后，重定向到之前请求的服务
             response = make_response(redirect(redirect_url))
             response.set_cookie(
@@ -57,7 +82,8 @@ def login():
         </form>
     """
 
-@app.route('/logout', methods=['GET'])
+
+@app.route("/logout", methods=["GET"])
 def logout():
     token = request.headers.get("Authorization", None)
     if token in tokens:
@@ -67,43 +93,50 @@ def logout():
     else:
         logging.warning(f"Attempt to logout with invalid or expired token: {token}")
         return "Invalid Token.", 403
-    
 
-@app.route("/verify", methods=["GET"])
+
+@app.route("/verify", methods=["POST"])
 def verify():
     # token = request.args.get('token', '')
     token = request.headers.get("Authorization", None)
-    
-    # decoded_token = jwt.decode(token, sso_secret_key, algorithms=['HS256'])
-    
-    token_info = tokens.get(token)
 
-    if not token_info:
-        logging.warning(f"Invalid token verification attempt: {token}")
+    # token_info = tokens.get(token)
+    try:
+        payload = jwt.decode(token, JWT_SECRET, algorithms=[JWT_ALGORITHM])
+    except jwt.PyJWTError:
+        logging.info("token verification failed")
         return jsonify({"status": "invalid"}), 403
 
-    if datetime.now() - token_info["issue_time"] > timedelta(minutes=TOKEN_EXPIRATION_MINUTES):
+    # if not token_info:
+    #     logging.warning(f"Invalid token verification attempt: {token}")
+    #     return jsonify({"status": "invalid"}), 403
+
+    if time.time() > payload["exp"]:
         # Token过期
-        del tokens[token]  # 清理过期Token
+        tokens.remove(token)  # 清理过期Token
         logging.warning(f"Expired token: {token}")
         return jsonify({"status": "expired"}), 403
 
     # Token有效
-    logging.info(f"Token {token} verified successfully for username: {token_info['username']}")
-    return jsonify({"status": "valid", "username": token_info["username"]})
+    logging.info(
+        f"Token {token} verified successfully for username: {payload['username']}"
+    )
+    return jsonify({"status": "valid", "username": payload["username"]})
 
-@app.route('/register', methods=['POST'])
+
+@app.route("/register", methods=["POST"])
 def register():
-    username = request.form.get('username')
-    password = request.form.get('password')
-    
+    username = request.form.get("username")
+    password = request.form.get("password")
+
     if username in users:
         logging.warning(f"Attempt to register with an existing username: {username}")
         return "Username already exists.", 400
-    
+
     users[username] = password
     logging.info(f"New user registered: {username}")
     return "User registered successfully.", 201
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000)
