20e2c5328092799c237f012bfe0021b2bdff83ba
maol <maol@nanofilm.com.cn>
2024-04-12 14:02:57 +0800

    🐞 fix(sso): sso
    v1, 每次验证通过装饰器调用 verify 接口

--- sso/py/app1.py
+++ sso/py/app1.py
@@ -1,5 +1,4 @@
 from functools import wraps
-import time
 from flask import Flask, g, request, redirect, jsonify
 import requests
 
@@ -15,9 +14,6 @@ JWT_SECRET = "sso-3E0C07FFFCFFF3E00E0039FCE00E7F387"  # 加解密密钥
 JWT_ALGORITHM = "HS256"  # 加解密算法
 
 
-# 使用字典作为简单的缓存
-token_cache = {}
-
 def token_required(f):
     @wraps(f)
     def decorated(*args, **kwargs):
@@ -25,19 +21,13 @@ def token_required(f):
         if not token:
             return redirect("http://localhost:8000/login?redirect_url=http://localhost:8001")
 
-        # 检查缓存
-        cache_entry = token_cache.get(token)
-        if cache_entry and cache_entry['expire_time'] > time.time():
-            g.username = cache_entry['username']
+        headers = {'Authorization': token}
+        response = requests.post(
+            'http://localhost:8000/verify', headers=headers)
+        if response.status_code == 200 and response.json()['status'] == 'valid':
+            g.username = response.json().get('username')
         else:
-            headers = {'Authorization': token}
-            response = requests.get('http://localhost:8000/verify', headers=headers)
-            if response.status_code == 200 and response.json()['status'] == 'valid':
-                g.username = response.json().get('username')
-                # 更新缓存，这里假设 token 有效期为 5 分钟
-                token_cache[token] = {'username': g.username, 'expire_time': time.time() + 300}
-            else:
-                return redirect("http://localhost:8000/login?redirect_url=http://localhost:8001")
+            return redirect("http://localhost:8000/login?redirect_url=http://localhost:8001")
         return f(*args, **kwargs)
     return decorated
 
@@ -45,7 +35,6 @@ def token_required(f):
 @app.route("/logout")
 @token_required
 def logout():
-    # 假设从某处获取到了Token，例如从session或者直接从请求中
     token = request.cookies.get("auth_token")
     # 向认证中心发送登出请求
     headers = {"Authorization": token}
@@ -82,7 +71,8 @@ def change_password():
 
     if token and new_password:
         # 验证Token
-        response = requests.get("http://localhost:8000/verify", params={"token": token})
+        response = requests.get(
+            "http://localhost:8000/verify", params={"token": token})
         if response.status_code == 200:
             username = response.json().get("username")
             # 简化示例，直接更新内存中的密码
--- sso/py/app2.py
+++ sso/py/app2.py
@@ -1,48 +1,40 @@
 from functools import wraps
 from flask import Flask, g, request, redirect, jsonify
 import requests
-from jwt import decode, ExpiredSignatureError, InvalidTokenError
-import jwt
+
+
 app = Flask(__name__)
 # 假设用户信息存储
 users_info = {
-    'user1': {
-        'email': 'user1@example.com',
-        'password': '123',
-        'name': 'User 1'
-    },
-    'user2': {
-        'email': 'user2@example.com',
-        'password': '123',
-        'name': 'User 2'
-    }
+    "user1": {"email": "user1@example.com", "password": "123", "name": "User 1"},
+    "user2": {"email": "user2@example.com", "password": "123", "name": "User 2"},
 }
+
+JWT_SECRET = "sso-3E0C07FFFCFFF3E00E0039FCE00E7F387"  # 加解密密钥
+JWT_ALGORITHM = "HS256"  # 加解密算法
+
 
 def token_required(f):
     @wraps(f)
     def decorated(*args, **kwargs):
         token = request.cookies.get("auth_token")
         if not token:
-            # 如果没有Token，重定向到登录页面
-            return redirect(
-                "http://localhost:8000/login?redirect_url=http://localhost:8002"
-            )
+            return redirect("http://localhost:8000/login?redirect_url=http://localhost:8002")
 
         headers = {'Authorization': token}
-        response = requests.post('http://localhost:8000/verify', headers=headers)
+        response = requests.post(
+            'http://localhost:8000/verify', headers=headers)
         if response.status_code == 200 and response.json()['status'] == 'valid':
             g.username = response.json().get('username')
         else:
             return redirect("http://localhost:8000/login?redirect_url=http://localhost:8002")
-
         return f(*args, **kwargs)
-
     return decorated
+
 
 @app.route("/logout")
 @token_required
 def logout():
-    # 假设从某处获取到了Token，例如从session或者直接从请求中
     token = request.cookies.get("auth_token")
     # 向认证中心发送登出请求
     headers = {"Authorization": token}
@@ -52,13 +44,16 @@ def logout():
         return f"Logout successfully!"
     else:
         # Token无效，重定向到登录页面
-        return redirect("http://localhost:8000/login?redirect_url=http://localhost:8002")
+        return redirect(
+            "http://localhost:8000/login?redirect_url=http://localhost:8002"
+        )
+
 
 @app.route("/")
 @token_required
 def home():
     username = g.username
-    return f"This is app1, Welcome {username}! You are logged in."
+    return f"This is app2, Welcome {username}! You are logged in."
 
 
 @app.route("/profile")
@@ -68,19 +63,21 @@ def profile():
     user_info = users_info.get(username, {})
     return jsonify(user_info)
 
-@app.route('/change-password', methods=['POST'])
+
+@app.route("/change-password", methods=["POST"])
 def change_password():
-    token = request.args.get('token', None)
-    new_password = request.form.get('new_password', None)
+    token = request.args.get("token", None)
+    new_password = request.form.get("new_password", None)
 
     if token and new_password:
         # 验证Token
-        response = requests.get('http://localhost:8000/verify', params={'token': token})
+        response = requests.get(
+            "http://localhost:8000/verify", params={"token": token})
         if response.status_code == 200:
-            username = response.json().get('username')
+            username = response.json().get("username")
             # 简化示例，直接更新内存中的密码
             if username in users_info:
-                users_info[username]['password'] = new_password
+                users_info[username]["password"] = new_password
                 return jsonify({"message": "Password changed successfully."})
             else:
                 return jsonify({"error": "User not found."}), 404
@@ -89,5 +86,6 @@ def change_password():
     else:
         return jsonify({"error": "Missing token or new password."}), 400
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8002)
